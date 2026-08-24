82294d06ebab5b240e29a7b6f53211b36536b5fc
Jonathan Sharman <jon.w.sharman@gmail.com>
2026-08-24 16:09:35 -0700

    chore: deployment works but nginx hangs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS control plane."
   type        = string
-  default     = "1.30"
+  default     = "1.31"
 }
 
 # --- Platform-managed networking --------------------------------------------
